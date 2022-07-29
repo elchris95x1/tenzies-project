8cd59f928c3c149afdbaa0f96c3cb8d970965844
Chris <achris95x1@gmail.com>
2022-07-29 00:16:18 -0500

    implement the random numbers
    Implement a function that produces an array of random number to be assigned to each dice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import './App.css';
 import Die from './components/die'
+import React from "react"
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
 
-  function allNewDice(){
-    
-    return(
-      <div>
-
-      </div>
-    )
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
   }
 
+  const diceElements = dice.map(die => <Die value={die} />)
   return (
-      <main>
-        <div class="dice-container">
-            <Die value="1" />
-            <Die value="2" />
-            <Die value="3" />
-            <Die value="4" />
-            <Die value="5" />
-            <Die value="6" />
-            <Die value="7" />
-            <Die value="8" />
-            <Die value="9" />
-            <Die value="10" />
-        </div>
-      </main>
+    <main>
+      <div class="dice-container">
+        {diceElements}
+      </div>
+    </main>
   );
 }
 
